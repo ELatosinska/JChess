9a4c92f8b0f3050a6148bf6bfe574e1df86deccb
Ela <ela.latosinska@gmail.com>
2022-09-30 12:23:55 +0200

    test commit

--- src/com/chess/engine/Alliance.java
+++ src/com/chess/engine/Alliance.java
@@ -4,6 +4,9 @@ import com.chess.engine.player.BlackPlayer;
 import com.chess.engine.player.Player;
 import com.chess.engine.player.WhitePlayer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum Alliance {
     WHITE{
         @Override
@@ -19,6 +22,15 @@ public enum Alliance {
         @Override
         public boolean isBlack() {
             return false;
+        }
+
+        @Override
+        public boolean isPawnPromotionSquare(int position) {
+            final List<Integer> promotionSquares = new ArrayList<>();
+            for(int i = 0; i<8; i++) {
+                promotionSquares.add(i);
+            }
+            return promotionSquares.contains(position);
         }
 
         @Override
@@ -48,6 +60,15 @@ public enum Alliance {
         }
 
         @Override
+        public boolean isPawnPromotionSquare(int position) {
+            final List<Integer> promotionSquares = new ArrayList<>();
+            for(int i = 56; i<64; i++) {
+                promotionSquares.add(i);
+            }
+            return promotionSquares.contains(position);
+        }
+
+        @Override
         public Player choosePlayer(final WhitePlayer whitePlayer,
                                    final BlackPlayer blackPlayer) {
             return blackPlayer;
@@ -62,6 +83,8 @@ public enum Alliance {
     public abstract int getOppositeDirection();
     public abstract boolean isWhite();
     public abstract boolean isBlack();
+
+    public abstract boolean isPawnPromotionSquare(int position);
 
     public abstract Player choosePlayer(WhitePlayer whitePlayer, BlackPlayer blackPlayer);
 
