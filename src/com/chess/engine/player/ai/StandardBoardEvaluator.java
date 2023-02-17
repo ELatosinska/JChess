84a27d6ee453f26c403d47463dc446931ae340fb
Ela <ela.latosinska@gmail.com>
2023-02-17 14:18:03 +0100

    test

---
+++ src/com/chess/engine/player/ai/StandardBoardEvaluator.java
@@ -0,0 +1,10 @@
+package com.chess.engine.player.ai;
+
+import com.chess.engine.board.Board;
+
+public class StandardBoardEvaluator implements BoardEvaluator {
+    @Override
+    public int evaluate(Board board, int depth) {
+        return 0;
+    }
+}
